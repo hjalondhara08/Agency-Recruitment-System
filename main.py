0fcf64a3109e2127f5dc89dd3647c148740e1dd9
HIREN JALONDHARA <jalondharahiren08@gmail.com>
2025-12-04 13:13:08 +0530

    c1

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from langchain_community.chat_models import ChatOpenAI
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
+
+
+model = ChatOpenAI(
+    model="openai/gpt-oss-safeguard-20b", 
+    temperature=1,
+    openai_api_base="https://openrouter.ai/api/v1",
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+)
+
+
+response = model.invoke("Hello")
+print(response)
