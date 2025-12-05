1d9a05cd8f7934cad8e4a47913b6edefc9ddecb1
hjalondhara08 <jalondharahiren08@gmail.com>
2025-12-05 21:08:29 +0530

    Comment out OpenAI API key and model usage
    Comment out OpenAI model initialization and API key setup.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import os
-from langchain_community.chat_models import ChatOpenAI
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
+# import os
+# from langchain_community.chat_models import ChatOpenAI
+# os.environ["OPENAI_API_KEY"] = "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"
+# os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
 
 
-model = ChatOpenAI(
-    model="openai/gpt-oss-safeguard-20b", 
-    temperature=1,
-    openai_api_base="https://openrouter.ai/api/v1",
-    openai_api_key=os.getenv("OPENAI_API_KEY"),
-)
+# model = ChatOpenAI(
+#     model="openai/gpt-oss-safeguard-20b", 
+#     temperature=1,
+#     openai_api_base="https://openrouter.ai/api/v1",
+#     openai_api_key=os.getenv("OPENAI_API_KEY"),
+# )
 
 
-response = model.invoke("who is prime minister of india")
-print(response)
+# response = model.invoke("who is prime minister of india")
+# print(response)
+
+
+print("hello how are you ")
